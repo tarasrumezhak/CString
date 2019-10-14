a9dd94e0ff057b86e945803c510a5353a106a133
tarasrumezhak <44346144+tarasrumezhak@users.noreply.github.com>
2019-10-14 15:35:17 +0300

    deleted txt

--- library.c
+++ library.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "library.h"
 
 typedef struct
@@ -6,3 +7,28 @@ typedef struct
     size_t size_m;	   // Фактичний розмір стрічки
     char*  data;	   // Вказівник на блок пам'яті
 } my_str_t;
+
+int my_str_create(my_str_t *str, size_t buf_size) {
+    str ->capacity_m = buf_size + 1;
+    str ->size_m = 0;
+    str->data = (char *) calloc(str->capacity_m, sizeof(char));
+    if (str->data == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+
+void my_str_free(my_str_t* str){
+    free(str ->data);
+}
+
+
+int my_str_from_cstr(my_str_t* str, const char* cstr, size_t buf_size){
+    if (buf_size == 0) {
+        str ->capacity_m = buf_size + 1;
+    }
+    else if (buf_size < sizeof(str)){
+        return -1;
+    }
+}
